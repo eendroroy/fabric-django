740d370dc6d78a795c0079084fa7abd83ac9df46
indrajit <eendroroy@gmail.com>
2018-01-21 23:43:36 +0600

    refactor

--- fabfile.py
+++ fabfile.py
@@ -5,10 +5,7 @@ from config.fabric.tasks.deploy import task_deploy
 from config.fabric.tasks.generate_sudoers import task_generate_sudoers
 from config.fabric.tasks.setup import task_setup
 
-# env.hosts = ['128.199.199.111']
-# env.user = 'forge'
-
-env.project_url = 'git@github.com:openhealthalgorithms/ohaApiDjango.git'
+env.project_url = 'git@github.com:deployer/django-app.git'
 env.branch = 'master'
 env.hosts = ['192.168.33.10']
 env.app_port = '8000'
@@ -16,10 +13,10 @@ env.uwsgi_process = 4
 env.uwsgi_threads = 2
 env.user = 'deployer'
 env.key_filename = '~/.ssh/id_rsa'
-env.deploy_key_name = 'ohaApiDjango'
+env.deploy_key_name = 'deploy_key'
 env.forward_agent = True
 
-env.app_name = 'ohaApiDjango'
+env.app_name = 'django-app'
 
 env.socket = f'/tmp/{env.app_name}.sock'
 
