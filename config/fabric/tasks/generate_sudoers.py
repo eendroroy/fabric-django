ac6b20a38e96b99534b0111601b2450215dd69e9
indrajit <eendroroy@gmail.com>
2018-01-21 23:04:52 +0600

    update fagric tasks

--- config/fabric/tasks/generate_sudoers.py
+++ config/fabric/tasks/generate_sudoers.py
@@ -1,9 +1,11 @@
+from fabric.operations import local
 from fabric.state import env
 
 from config.fabric.template_render import __render_template
 
 
 def task_generate_sudoers():
+    local(f'mkdir -p {env.local_path}/tmp/sudoer/')
     sudoer_file_path = f'{env.local_path}/tmp/sudoer/{env.user}'
     sudoer_file = open(sudoer_file_path, 'w+')
     sudoer_file.write(__render_template(f'{env.local_path}/config/deploy/templates/sudoer.j2'))
